63788bb319edec90e8138e8c641e7c0a14abeae2
jsmoon <jsmoon2225@gmail.com>
2024-12-17 09:22:57 +0900

    메인페이지 검색 기능 구현 완료

--- src/main/java/com/kream/kream/configs/RedisConfig.java
+++
@@ -1,8 +0,0 @@
-package com.kream.kream.configs;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RedisConfig {
-
-}
--- src/main/java/com/kream/kream/entities/BuyerBidEntity.java
+++ src/main/java/com/kream/kream/entities/BuyerBidEntity.java
@@ -4,7 +4,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
